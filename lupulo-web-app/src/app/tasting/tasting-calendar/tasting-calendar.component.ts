ca9cc04f65521f19ce62a1ae4bf42c592212f09c
Pedro <pedro.marnau@gmail.com>
2022-04-11 19:42:09 +0200

    inicializando testet

--- lupulo-web-app/src/app/tasting/tasting-calendar/tasting-calendar.component.ts
+++ lupulo-web-app/src/app/tasting/tasting-calendar/tasting-calendar.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasting-calendar.component.css']
 })
 export class TastingCalendarComponent implements OnInit {
+  selected: Date | null;
+  minDate: Date;
+  maxDate: Date;
 
-  constructor() { }
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 2;
+  };
+
+  constructor() {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date();
+    this.maxDate = new Date(currentYear + 1, 11, 31);
+   }
 
   ngOnInit(): void {
   }
